b9e45a2d467517d5d8e8f3c9355233c87e72fc0c
kimdaeh <popretty@gmail.com>
2020-03-09 02:43:36 -0700

    change function

--- R/change.R
+++ R/change.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,12 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+change <- function (data) {
+  var.name <- data[ , 1]
+  data <- data[ , -1]
+  data <- as.data.frame( t(data) )
+  rownames(data) <- c(1 : 5)
+  colnames(data) <- var.name
+  colnames(data)[1] <- "Year"
+  data
 }
